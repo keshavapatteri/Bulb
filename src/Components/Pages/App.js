2b32f6c3350cf25686bd41acc72e80c21bae647a
keshavapatteri <mktkid2023@gmail.com>
2024-06-14 15:44:41 +0530

    first commit

---
+++ src/Components/Pages/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Bulb from './Bulb';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Bulb App</h1>
+                <Bulb />
+            </header>
+        </div>
+    );
+}
+
+export default App;
